3ee560a098c8da69fdd838c91be25eae4b5d3561
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-11-24 16:51:56 -0700

    Add structs and implementation for neural network

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,3 +1,82 @@
+use rand::Rng;
+
+pub struct Network {
+    layers: Vec<Layer>,
+}
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+struct Neuron {
+    weights: Vec<f32>,
+    bias: f32,
+}
+
+pub struct LayerTopology {
+    pub neurons: usize,
+}
+
+impl Network {
+    pub fn random(rng: &mut dyn rand::RngCore, layers: &Vec<LayerTopology>) -> Network {
+        assert!(layers.len() > 1);
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
+            .collect();
+
+        Network { layers }
+    }
+
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
+impl Layer {
+    pub fn random(
+        rng: &mut dyn rand::RngCore,
+        input_neurons: usize,
+        output_neurons: usize,
+    ) -> Layer {
+        let neurons = (0..output_neurons)
+            .map(|_| Neuron::random(rng, input_neurons))
+            .collect();
+
+        Layer { neurons }
+    }
+
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+}
+
+impl Neuron {
+    pub fn random(rng: &mut dyn rand::RngCore, output_size: usize) -> Neuron {
+        let weights = (0..output_size)
+            .map(|_| rng.gen_range(-1.0..=1.0))
+            .collect();
+
+        let bias = rng.gen_range(-1.0..=1.0);
+
+        Neuron { weights, bias }
+    }
+
+    fn propagate(&self, inputs: &Vec<f32>) -> f32 {
+        let sum = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| weight * input)
+            .sum::<f32>();
+
+        (sum + self.bias).max(0.0)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
