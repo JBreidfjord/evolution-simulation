2978d29df4485f0b5d52afa20ce199b0919837b2
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-11-25 14:10:01 -0700

    Define traits for Chromosome

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,4 +1,8 @@
+#![feature(type_alias_impl_trait)]
+
 use rand::seq::SliceRandom;
+use std::iter::FromIterator;
+use std::ops::Index;
 
 pub struct GeneticAlgorithm<S> {
     selection_method: S,
@@ -61,6 +65,45 @@ impl SelectionMethod for RouletteWheelSelection {
         population
             .choose_weighted(rng, |i| i.fitness())
             .expect("Received empty population")
+    }
+}
+
+impl Chromosome {
+    pub fn len(&self) -> usize {
+        self.genes.len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &f32> {
+        self.genes.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut f32> {
+        self.genes.iter_mut()
+    }
+}
+
+impl Index<usize> for Chromosome {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.genes[index]
+    }
+}
+
+impl FromIterator<f32> for Chromosome {
+    fn from_iter<T: IntoIterator<Item = f32>>(iter: T) -> Chromosome {
+        Chromosome {
+            genes: iter.into_iter().collect(),
+        }
+    }
+}
+
+impl IntoIterator for Chromosome {
+    type Item = f32;
+    type IntoIter = impl Iterator<Item = f32>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.genes.into_iter()
     }
 }
 
