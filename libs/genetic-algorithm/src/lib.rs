f26ad563ad2a291f016eb8959a93103761c54765
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-11-25 14:09:46 -0700

    Define Chromosome struct & tests; refactor tests

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -6,6 +6,11 @@ pub struct GeneticAlgorithm<S> {
 
 pub struct RouletteWheelSelection;
 
+#[derive(Clone, Debug)]
+pub struct Chromosome {
+    genes: Vec<f32>,
+}
+
 pub trait Individual {
     fn fitness(&self) -> f32;
 }
@@ -82,37 +87,95 @@ impl Individual for TestIndividual {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rand::SeedableRng;
-    use rand_chacha::ChaCha8Rng;
-    use std::collections::BTreeMap;
 
-    #[test]
-    fn test() {
-        let method = RouletteWheelSelection::new();
-        let mut rng = ChaCha8Rng::from_seed(Default::default());
+    mod selection {
+        use super::*;
 
-        let population = vec![
-            TestIndividual::new(1.0),
-            TestIndividual::new(2.0),
-            TestIndividual::new(3.0),
-            TestIndividual::new(4.0),
-        ];
+        mod roulettewheelselection {
+            use super::*;
+            use rand::SeedableRng;
+            use rand_chacha::ChaCha8Rng;
+            use std::collections::BTreeMap;
 
-        let actual_histogram: BTreeMap<i32, _> = (0..1000)
-            .map(|_| method.select(&mut rng, &population))
-            .fold(Default::default(), |mut histogram, individual| {
-                *histogram.entry(individual.fitness() as _).or_default() += 1;
-                histogram
-            });
+            #[test]
+            fn test() {
+                let method = RouletteWheelSelection::new();
+                let mut rng = ChaCha8Rng::from_seed(Default::default());
 
-        let expected_histogram = maplit::btreemap! {
-            // fitness => selection count
-            1 => 102,
-            2 => 198,
-            3 => 301,
-            4 => 399,
-        };
+                let population = vec![
+                    TestIndividual::new(1.0),
+                    TestIndividual::new(2.0),
+                    TestIndividual::new(3.0),
+                    TestIndividual::new(4.0),
+                ];
 
-        assert_eq!(actual_histogram, expected_histogram);
+                let actual_histogram: BTreeMap<i32, _> = (0..1000)
+                    .map(|_| method.select(&mut rng, &population))
+                    .fold(Default::default(), |mut histogram, individual| {
+                        *histogram.entry(individual.fitness() as _).or_default() += 1;
+                        histogram
+                    });
+
+                let expected_histogram = maplit::btreemap! {
+                    // fitness => selection count
+                    1 => 102,
+                    2 => 198,
+                    3 => 301,
+                    4 => 399,
+                };
+
+                assert_eq!(actual_histogram, expected_histogram);
+            }
+        }
+    }
+
+    mod chromosome {
+        use super::*;
+
+        mod index {
+            use super::*;
+
+            #[test]
+            fn test() {
+                let chromosome = Chromosome {
+                    genes: vec![1.0, 2.0, 3.0],
+                };
+
+                assert_eq!(chromosome[0], 1.0);
+                assert_eq!(chromosome[1], 2.0);
+                assert_eq!(chromosome[2], 3.0);
+            }
+        }
+
+        mod from_iterator {
+            use super::*;
+
+            #[test]
+            fn test() {
+                let chromosome: Chromosome = vec![1.0, 2.0, 3.0].into_iter().collect();
+
+                assert_eq!(chromosome[0], 1.0);
+                assert_eq!(chromosome[1], 2.0);
+                assert_eq!(chromosome[2], 3.0);
+            }
+        }
+
+        mod into_iterator {
+            use super::*;
+
+            #[test]
+            fn test() {
+                let chromosome = Chromosome {
+                    genes: vec![1.0, 2.0, 3.0],
+                };
+
+                let genes: Vec<_> = chromosome.into_iter().collect();
+
+                assert_eq!(genes.len(), 3);
+                assert_eq!(genes[0], 1.0);
+                assert_eq!(genes[1], 2.0);
+                assert_eq!(genes[2], 3.0);
+            }
+        }
     }
 }
