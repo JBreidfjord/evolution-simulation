76dfafa5cd4d8acce7cb2e30e06a5cbba87c00f8
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-11-26 09:27:33 -0700

    Add tests for CrossoverMethod

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -139,7 +139,7 @@ mod tests {
     mod selection {
         use super::*;
 
-        mod roulettewheelselection {
+        mod roulettewheel {
             use super::*;
             use rand::SeedableRng;
             use rand_chacha::ChaCha8Rng;
@@ -173,6 +173,33 @@ mod tests {
                 };
 
                 assert_eq!(actual_histogram, expected_histogram);
+            }
+        }
+    }
+
+    mod crossover {
+        use super::*;
+
+        mod uniform {
+            use super::*;
+            use rand::SeedableRng;
+            use rand_chacha::ChaCha8Rng;
+
+            #[test]
+            fn test() {
+                let mut rng = ChaCha8Rng::from_seed(Default::default());
+                let parent_a: Chromosome = (1..=100).map(|n| n as f32).collect();
+                let parent_b: Chromosome = (1..=100).map(|n| -n as f32).collect();
+                let child = UniformCrossover::new().crossover(&mut rng, &parent_a, &parent_b);
+
+                // Number of genes different between 'child' and 'parent_a'
+                let diff_a = child.iter().zip(parent_a).filter(|(c, p)| *c != p).count();
+
+                // Number of genes different between 'child' and 'parent_b'
+                let diff_b = child.iter().zip(parent_b).filter(|(c, p)| *c != p).count();
+
+                assert_eq!(diff_a, 49);
+                assert_eq!(diff_b, 51);
             }
         }
     }
