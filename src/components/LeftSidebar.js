d371bccf015bb247bb8f00e3ad838a4548810d2e
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-29 22:59:05 -0700

    Prevent Sidebar close on clicking other elements

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -5,10 +5,17 @@ import { useState } from "react";
 export default function LeftSidebar({ children }) {
   const [open, setOpen] = useState("");
 
+  const handleClose = (e) => {
+    e.preventDefault();
+    if (e.currentTarget === e.target) {
+      setOpen("");
+    }
+  };
+
   return (
     <>
       <div className="sidebar-toggle left" onClick={() => setOpen("open")}></div>
-      <div className={"sidebar left" + ` ${open}`} onClick={() => setOpen("")}>
+      <div className={"sidebar left" + ` ${open}`} onClick={handleClose}>
         {open && children}
       </div>
     </>
--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -5,10 +5,17 @@ import { useState } from "react";
 export default function RightSidebar({ children }) {
   const [open, setOpen] = useState("");
 
+  const handleClose = (e) => {
+    e.preventDefault();
+    if (e.currentTarget === e.target) {
+      setOpen("");
+    }
+  };
+
   return (
     <>
       <div className="sidebar-toggle right" onClick={() => setOpen("open")}></div>
-      <div className={"sidebar right" + ` ${open}`} onClick={() => setOpen("")}>
+      <div className={"sidebar right" + ` ${open}`} onClick={handleClose}>
         {open && children}
       </div>
     </>
